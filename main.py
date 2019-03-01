20b4b644265f3119d9ee50d69ad546748b5ae403
valhentine <138431565+valhentine@users.noreply.github.com>
2019-02-28 21:56:50 -0500

    inital

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+
+TOKEN = 'NTUwODU1MTUxODkyNjkyOTky.D1ovfA._13Nmqjkh01I_Q9b8I_wPX9mtBA'
+client = discord.Client()
+
+class Server():
+    pass
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
