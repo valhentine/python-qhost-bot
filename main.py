b40fbf4b646aff88566e8f2a61d03ffc98904f1c
valhentine <138431565+valhentine@users.noreply.github.com>
2019-03-03 15:26:40 -0500

    init 2

--- main.py
+++ main.py
@@ -1,47 +1,40 @@
 import discord
+import os
 
 TOKEN = 'NTUwODU1MTUxODkyNjkyOTky.D1ovfA._13Nmqjkh01I_Q9b8I_wPX9mtBA'
 client = discord.Client()
+directory = "D:\servers\sapphire\isle"
 
-class Server():
-    def __init__(self, name, port, directory):
-        self.commands = []
-        self.roles = []
-        self.channels = []
-        self.port = port
-        self.name = name
-        self.dir = directory
-        self.roles.insert(0, "Developer")
-        self.roles.insert(0, "Admin")
-    
-    def addCommand(self, command, output, channel):
-        self.commands.insert(0, [command, output, channel])
+def killServer():
+    pass
 
-    def addRole(self, role):
-        self.roles.insert(0, role)
+def startServer():
+    pass
 
-    def addChannel(self, channel):
-        self.channels.insert(0, channel)
-
-    def doCommand(self, command):
-        pass
-
-sapphireIsle = Server("Sapphire Isle", 27015, "D:\servers\sapphire\isle")
-sapphireIsle.addRole("Server Development")
-sapphireIsle.addChannel("sapphire-isle")
-sapphireIsle.addCommand(["", ""], "Send server start request", "sapphire-isle")
-
-servers = []
+def updateServer():
+    pass
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
+    if message.content.startswith('!channel'):
+        msg = str(message.channel)
+        msg = msg + ' {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
-        print("Hello World")
+
+    if str(message.channel) == 'sapphire-isle':
+        if message.content.startswith('!start'):
+            killServer()
+            startServer()
+        elif message.content.startswith('!stop'):
+            killServer()
+        elif message.content.startswith('!update'):
+            killServer()
+            updateServer()
+    elif str(message.channel) == 'admin':
+        pass
 
 @client.event
 async def on_ready():
