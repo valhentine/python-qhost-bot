76860f8b3ed5b975efdf00e075a327e3801e27b0
valhentine <138431565+valhentine@users.noreply.github.com>
2019-03-03 15:53:22 -0500

    Working

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ client = discord.Client()
 directory = "D:\servers\sapphire\isle"
 
 def killServer():
-    pass
+    os.system('taskkill /F /FI "WindowTitle eq Administrator:  qHost Isle Server" /T')
 
 def startServer():
-    pass
+    os.system('start cmd /c ' + directory + '\start.bat')
 
 def updateServer():
-    pass
+    os.system(directory + '\\update.bat')
 
 @client.event
 async def on_message(message):
@@ -25,14 +25,35 @@ async def on_message(message):
         await client.send_message(message.channel, msg)
 
     if str(message.channel) == 'sapphire-isle':
+        if message.content.startswith('!help') or message.content.startswith('!commands'):
+            msg = 'Commands:'
+            msg = msg + '```!start | Starts the server\n'
+            msg = msg + '!stop | Stops the server\n'
+            msg = msg + '!update | Updates the server\n'
+            msg = msg + '!restart | Restarts the server'
+            msg = msg + '```'.format(message)
+            await client.send_message(message.channel, msg)
         if message.content.startswith('!start'):
             killServer()
             startServer()
+            msg = '{0.author.mention} | Server Started'.format(message)
+            await client.send_message(message.channel, msg)
         elif message.content.startswith('!stop'):
             killServer()
+            msg = '{0.author.mention} | Server Stopped'.format(message)
+            await client.send_message(message.channel, msg)
         elif message.content.startswith('!update'):
             killServer()
+            msg = '{0.author.mention} | Server Update Started'.format(message)
+            await client.send_message(message.channel, msg)
             updateServer()
+            msg = '{0.author.mention} | Server Update Finished'.format(message)
+            await client.send_message(message.channel, msg)
+        elif message.content.startswith('!restart'):
+            killServer()
+            startServer()
+            msg = '{0.author.mention} | Server Restarted'.format(message)
+            await client.send_message(message.channel, msg)
     elif str(message.channel) == 'admin':
         pass
 
